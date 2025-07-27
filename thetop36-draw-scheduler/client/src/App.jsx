a28161024d0011c982270217b6d9298ec0e88906
999SHREYA <138154690+999SHREYA@users.noreply.github.com>
2025-07-27 10:52:01 +0530

    Update App.jsx

--- thetop36-draw-scheduler/client/src/App.jsx
+++ thetop36-draw-scheduler/client/src/App.jsx
@@ -1,22 +1,4 @@
-// import WinnerFeed from "./components/WinnerFeed";
 
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-background font-inter">
-//       
-
-//       <main>
-//         <WinnerFeed />
-//       </main>
-
-//       <footer className="p-4 text-xs text-center text-gray-500">
-//         © 2025 TheTop36.com
-//       </footer>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 import Header from './components/Header';
@@ -26,9 +8,7 @@ import ParticipantsList from './components/ParticipantsList';
 export default function App() {
   return (
     <div className="max-w-lg p-4 mx-auto">
-      <header className="p-4 text-center text-white bg-primary">
-       <img src="/assets/logo.svg" alt="Logo" className="h-10 mx-auto" />
-       </header>
+   
       <Header />
       <WinnerFeed />
       <ParticipantsList />
